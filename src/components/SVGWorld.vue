4405ead674a8bc4e71b0e65848e1d704d447a622
amedora <americandragsterracing@gmail.com>
2018-05-10 10:18:12 +0900

    dynamic line

--- src/components/SVGWorld.vue
+++ src/components/SVGWorld.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <svg viewbox="0 0 300 300" width="300" height="300">
+      <line :x1="x1" y1=100 x2=200 y2=200 stroke="black"></line>
+    </svg>
+    <div>
+      <input type="range" v-model="x1">
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -9,7 +16,8 @@ export default {
   name: 'SVGWorld',
   data () {
     return {
-      msg: 'Welcome to SVG World'
+      msg: 'Welcome to SVG World',
+      x1: 100
     }
   }
 }
